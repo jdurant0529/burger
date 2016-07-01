5b28bd99c936e03e5adaf9032c30e084f8441b09
jdurant0529 <james.durant@gmail.com>
2016-06-30 22:10:43 -0400

    all files created, need content.

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+use burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Quarter Pounder');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Royale with Cheese');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac');
