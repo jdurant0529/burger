0972f1d9d243c46359e806770d5c8475089a6a60
jdurant0529 <james.durant@gmail.com>
2016-07-07 13:08:48 -0400

    hku

--- models/burger.js
+++ models/burger.js
@@ -1,11 +1,3 @@
 var orm = require('../config/orm.js');
 
-// var eatenBurgers = orm.selectAll('burgers', 'devoured', true);
-
-// var createdBurgers = orm.selectAll('burgers', 'devoured', false);
-
-// var makeBurger =  orm.insertOne('burgers', 'burger_name', 'devoured', 'date', res.body.burger);
-
-// var eatDaBurger = orm.updateOne('burgers', 'devoured', 'id', res.body.id);
-
 module.exports = burger.js;
