37f130e1009c0245c86495105553ef5a896cc623
jdurant0529 <james.durant@gmail.com>
2016-07-07 11:18:04 -0400

    adding burgers works, try adding button to non-eaten burgers now.

--- config/orm.js
+++ config/orm.js
@@ -3,39 +3,43 @@ var connection = require('./connection.js');
 var orm = {
 
 
-	isEaten: function(){
-		return new Promise(function(resolve, reject){
-			var queryString = "SELECT * FROM burgers where devoured = '1'";
+	isEaten: function(table, cb){
+		
+			var queryString = "SELECT * FROM " + table + " where devoured = '1'";
 			connection.query(queryString, function(err, res){
 				if (err) throw err;
-				//console.log(res);
-				return resolve(res);
+				return cb(res);
 			});
-		})
+		
 	},
 
-	isNotEaten: function(){
-		return new Promise(function(resolve, reject){
-			var queryString = "SELECT * FROM burgers where devoured = '0'";
+	isNotEaten: function(table, cb){
+		
+			var queryString = "SELECT * FROM " + table + " where devoured = '0'";
 			connection.query(queryString, function(err, res){
 				if (err) throw err;
-				//console.log(res);
-				return resolve(res);
-			});
-		})
-	}
+				return cb(res);
+			});	
+	},
+	addBurger: function(table, newBurger, cb) {
+		
+		console.log("table: " +table);
+		console.log('newBurger: ' +newBurger);
 
-	// insertOne: function(table, newObject){
-	// 	return new Promise(function(resolve, reject){
-	// 		var queryString = 'INSERT INTO '+table+' SET ?'; 
-	// 		connection.query(queryString, newObject, function(err, res) {
- //      			if (err) throw err;
- //      			//console.log(res);
- //      			return resolve(res);
- //    		});
- //    	});	
-	// },
-	
+//var post  = {newBurger: newBurger, devoured: 'false'};
+//console.log('post: ' + post);
+
+// insert into burgers (burger_name, devoured) values ('What-a-burger', '0');
+
+			//var queryString = 'INSERT INTO ' + table + ' set ? {burger_name: "' + newBurger +'", devoured: "0" }';
+
+			var queryString = "INSERT INTO burgers SET ?', {burger_name: '"+newBurger + "', devoured: '0'}";
+			console.log('queryString: ' + queryString); 
+			connection.query("INSERT INTO burgers SET ?", {burger_name: newBurger, devoured: '0'}, function(err, res) {
+				if (err) throw err;
+					return cb(res);
+			})
+	}
 };
 
 module.exports = orm;
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -8,25 +8,39 @@ var app = express();
 
 // Creating Routes
 module.exports = function(app) {
-var devoured;
-var notDevoured;
+
+    var devoured;
+    var notDevoured;
 
     app.get('/', function(req, res) {
-        orm.isNotEaten('burgers').then(function(data) {
-                console.log(data);
+        orm.isNotEaten('burgers', function(data) {
+            console.log('data in isnot eaten: ' + data);
             notDevoured = data;
-            console.log('inside isNotEaten: ' + notDevoured);
+            console.log('notDevoured inside isNotEaten: ' + notDevoured);
+
+            orm.isEaten('burgers', function(data) {
+                console.log('data inside isEaten' + data);
+                devoured = data;
+                console.log('devoured inside isEaten: ' + devoured);
+                console.log('not devoured after app.gets: ' + notDevoured);
+                console.log('devoured after app.gets: ' + devoured);
+                res.render('index', { eaten: devoured, notEaten: notDevoured })
+            });
         });
 
-        orm.isEaten('burgers').then(function(data) {
-                console.log(data);
-            devoured = data;
-            console.log('inside isEaten: '+ devoured);
-            
-        });
-console.log('not devoured: ' + notDevoured);
-console.log('devoured: ' + devoured);
-        res.render('index', { eaten: devoured, notEaten: notDevoured})
+        app.post('/create', function (req, res) {
+                var body = req.body
+                console.log(body);
+                //console.log(burger);
+                orm.addBurger('burgers', req.body.burger, function (data){
+                      console.log('stuff was done.');  
+                      res.redirect('/');
+                })
+        })
+
+
+
+
     })
 
 }
