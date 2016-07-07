83b2490d89efc52870c4e260f9c50f778ee1600c
jdurant0529 <james.durant@gmail.com>
2016-07-07 12:55:32 -0400

    heroku trials

--- models/burger.js
+++ models/burger.js
@@ -1,11 +1,11 @@
 var orm = require('../config/orm.js');
 
-var eatenBurgers = orm.selectAll('burgers', 'devoured', true);
+// var eatenBurgers = orm.selectAll('burgers', 'devoured', true);
 
-var createdBurgers = orm.selectAll('burgers', 'devoured', false);
+// var createdBurgers = orm.selectAll('burgers', 'devoured', false);
 
-var makeBurger =  orm.insertOne('burgers', 'burger_name', 'devoured', 'date', res.body.burger);
+// var makeBurger =  orm.insertOne('burgers', 'burger_name', 'devoured', 'date', res.body.burger);
 
-var eatDaBurger = orm.updateOne('burgers', 'devoured', 'id', res.body.id);
+// var eatDaBurger = orm.updateOne('burgers', 'devoured', 'id', res.body.id);
 
 module.exports = burger.js;
